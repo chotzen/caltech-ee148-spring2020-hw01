0eff6a40381e42cfd2b566641b2952f962786d08
Devin Hartzell <devinhartzell@gmail.com>
2021-04-06 18:52:52 -0700

    final commit

---
+++ gen_imgs.py
@@ -0,0 +1,33 @@
+
+import os
+import numpy as np
+from tqdm import tqdm
+import json
+from PIL import Image
+from red_light_detector import RedLightDetector
+
+# set the path to the downloaded data:
+data_path = '../data/redlightdata'
+
+# set a path for saving predictions:
+preds_path = '../data/hw01_preds'
+
+os.makedirs(preds_path,exist_ok=True) # create directory if needed
+
+# get sorted list of files:
+file_names = sorted(os.listdir(data_path))
+
+# remove any non-JPEG files:
+file_names = [f for f in file_names if '.jpg' in f]
+
+preds = {}
+for i in [1, 11]:
+    rld = RedLightDetector()
+
+    # read image using PIL:
+    I = Image.open(os.path.join(data_path, file_names[i]))
+
+    # convert to numpy array:
+    I = np.asarray(I).astype(float)
+    preds[file_names[i]] = rld.detect_red_light(I, display_image=True)
+
